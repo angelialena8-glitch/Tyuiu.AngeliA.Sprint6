3c257a3a4d7f83a285f994193626ce6eb418af4b
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-14 15:07:29 +0500

    Task4

---
+++ Tyuiu.AngeliA.Sprint6.Task4.V7.Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.AngeliA.Sprint6.Task4.V7.Lib;
+namespace Tyuiu.AngeliA.Sprint6.Task4.V7.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.AngeliA.Sprint6.Task4.V7/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.AngeliA.Sprint6.Task4.V7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
