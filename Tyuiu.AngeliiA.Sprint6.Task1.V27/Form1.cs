475069c9e40989b2d218598571eb01a858397a1e
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-14 14:46:02 +0500

    Task1

---
+++ Tyuiu.AngeliiA.Sprint6.Task1.V27/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.AngeliiA.Sprint6.Task1.V27
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
