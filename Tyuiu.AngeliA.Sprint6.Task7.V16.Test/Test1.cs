e64628e777d7a4143dc050cb79b3515f35ab7ddc
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-14 15:59:34 +0500

    Task7

---
+++ Tyuiu.AngeliA.Sprint6.Task7.V16.Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.AngeliA.Sprint6.Task7.V16.Lib;
+namespace Tyuiu.AngeliA.Sprint6.Task7.V16.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.AngeliA.Sprint6.Task7.V16/Program.cs
@@ -0,0 +1,18 @@
+using Tyuiu.AngeliA.Sprint6.Task7.V16.Lib;
+namespace Tyuiu.AngeliA.Sprint6.Task7.V16
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
